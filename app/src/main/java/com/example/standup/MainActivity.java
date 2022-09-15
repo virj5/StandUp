30ee70a3b97bf661955a1297cb499a13f74871c8
virkinASC4 <vjimenez14x@gmail.com>
2022-09-15 11:50:43 -0500

    CodeLabs 8.2 : Alarm Manager

---
+++ app/src/main/java/com/example/standup/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.standup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManager mNotificationManager;
+
+    //these will help us display the notification
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        //initialize notification manager
+        mNotificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        //change listener being set
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                String toastMessage;
+                if(b){
+                    //send the notification to be displayed
+                    deliverNotification(MainActivity.this);
+                    toastMessage = getString(R.string.on_case);
+                }
+                else{
+                    //cancel the notification if it is turned off
+                    mNotificationManager.cancelAll();
+                    toastMessage = getString(R.string.off_case);
+                }
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        createNotificationChannel();
+
+    }
+
+    /**
+     * Creates a Notification channel to display notification
+     */
+    public void createNotificationChannel() {
+
+        // Create a notification manager object.
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context){
+        //used to display the notification content
+        Intent contentIntent = new Intent(context, MainActivity.class);
+
+        //tells the system how to handle when multiple instances of the same PendingIntent
+        //are created.
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)   //walk icon used for stand up (created in drawable)
+                .setContentTitle("Stand Up Alert")
+                .setContentText("You should stand up and walk around now!")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        //this line delivers the notification
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
